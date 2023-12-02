39c87dafbbd0886f60b2d8cea0f494a9a818d10f
Роман Меркулов <merkulov@trueconf.ru>
2023-12-02 09:01:56 +0300

    init

--- main.go
+++ main.go
@@ -3,122 +3,53 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type Set []struct {
-	IndexFirst int
-	IndexLast  int
-	Num        int
+type Game struct {
+	Number int
+	Sets   []Set
+}
+
+type Set struct {
+	R int
+	G int
+	B int
 }
 
 func main() {
 	input, _ := os.ReadFile("input")
-	var sum int
 	scanner := bufio.NewScanner(strings.NewReader(string(input)))
 	for scanner.Scan() {
-		inputString := scanner.Text()
-		var Index Set
-		for _, num := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9} {
-			tempIndexFirst := strings.Index(inputString, strconv.Itoa(num))
-			tempIndexLast := strings.LastIndex(inputString, strconv.Itoa(num))
-			if tempIndexFirst > -1 || tempIndexLast > -1 {
-				Index = append(Index, struct {
-					IndexFirst int
-					IndexLast  int
-					Num        int
-				}{
-					IndexFirst: tempIndexFirst,
-					IndexLast:  tempIndexLast,
-					Num:        num,
-				})
-			}
-		}
-		for _, num := range []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"} {
-			tempIndexFirst := strings.Index(inputString, num)
-			tempIndexLast := strings.LastIndex(inputString, num)
-			if tempIndexFirst > -1 || tempIndexLast > -1 {
-				Index = append(Index, struct {
-					IndexFirst int
-					IndexLast  int
-					Num        int
-				}{
-					IndexFirst: tempIndexFirst,
-					IndexLast:  tempIndexLast,
-					Num:        Replacer(num),
-				})
-			}
-		}
-		first, last := index2num(FindFirstIndex(Index), FindLastIndex(Index), Index)
-		if first == last {
-			log.Println("sdfsdf")
-		}
-		total, _ := strconv.Atoi(fmt.Sprintf("%s%s", first, last))
-
-		sum = sum + total
+		game := GameParse(scanner.Text())
+		fmt.Println(game)
 	}
-	log.Printf("total %d", sum)
+
 }
 
-func Replacer(input string) int {
-	switch input {
-	case "one":
-		return 1
-	case "two":
-		return 2
-	case "three":
-		return 3
-	case "four":
-		return 4
-	case "five":
-		return 5
-	case "six":
-		return 6
-	case "seven":
-		return 7
-	case "eight":
-		return 8
-	case "nine":
-		return 9
-	default:
-		return 0
+func GameParse(input string) (game Game) {
+	cutOne := strings.Split(input, ":")
+	game.Number, _ = strconv.Atoi(strings.Fields(cutOne[0])[1])
+	setsBody := strings.Split(strings.ReplaceAll(cutOne[1], " ", ""), ";")
+	for _, set := range setsBody {
+		setParsed := SetParse(set)
+		game.Sets = append(game.Sets, struct {
+			R int
+			G int
+			B int
+		}{
+			R: setParsed.R,
+			G: setParsed.G,
+			B: setParsed.B,
+		})
 	}
+	return game
 }
 
-func index2num(indexFirst int, indexLast int, List Set) (first, last string) {
-	var firstItem int
-	var lastItem int
-	for _, item := range List {
-		if indexFirst == item.IndexFirst {
-			firstItem = item.Num
-		}
-		if indexLast == item.IndexLast {
-			lastItem = item.Num
-		}
-	}
-
-	return strconv.Itoa(firstItem), strconv.Itoa(lastItem)
-}
-
-func FindFirstIndex(Index Set) (first int) {
-	first = 999
-	for _, item := range Index {
-		if item.IndexFirst < first {
-			first = item.IndexFirst
-		}
-	}
-	return first
-}
-
-func FindLastIndex(Index Set) (last int) {
-	last = 0
-	for _, item := range Index {
-		if item.IndexLast > last {
-			last = item.IndexLast
-		}
-	}
-	return last
+func SetParse(input string) (set Set) {
+	cutOne := strings.Split(input, ",")
+	fmt.Println(cutOne)
+	return set
 }
