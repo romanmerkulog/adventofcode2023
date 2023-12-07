9fdb942982c8ce21fdebec3a9b794221331d2caf
Роман Меркулов <merkulov@trueconf.ru>
2023-12-07 20:15:26 +0300

    horror pt2

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 					if temp[i].Card[2] == temp[j].Card[2] {
 						if temp[i].Card[3] == temp[j].Card[3] {
 							if temp[i].Card[4] == temp[j].Card[4] {
-								return temp[i].Card[0] < temp[j].Card[0]
+								return temp[i].Card[5] < temp[j].Card[5]
 							}
 							return temp[i].Card[4] < temp[j].Card[4]
 						}
@@ -58,6 +58,7 @@ func main() {
 		})
 		for _, each := range temp {
 			totalSum += each.Bid * multiplier
+			fmt.Println(each.Label, each.Bid)
 			multiplier++
 		}
 	}
@@ -77,40 +78,47 @@ func GetInput() (result []string) {
 func ParseHand(input string) (hand Hand) {
 	handString := strings.Split(input, " ")[0]
 	hand.Bid, _ = strconv.Atoi(strings.Split(input, " ")[1])
-	for _, card := range []string{"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"} {
-		if strings.Count(handString, card) > 0 {
-			hand.Set = append(hand.Set, struct {
-				Card  string
-				Count int
-			}{Card: card, Count: strings.Count(handString, card)})
+	var Joker = strings.Count(handString, "J")
+	if Joker == 0 {
+		for _, card := range []string{"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"} {
+			if strings.Count(handString, card) > 0 {
+				hand.Set = append(hand.Set, struct {
+					Card  string
+					Count int
+				}{Card: card, Count: strings.Count(handString, card)})
+			}
 		}
+		if len(hand.Set) == 5 {
+			hand.Kind = 1
+		} else if len(hand.Set) == 4 {
+			hand.Kind = 2
+		} else if len(hand.Set) == 3 {
+			if hand.Set[0].Count == 3 || hand.Set[1].Count == 3 || hand.Set[2].Count == 3 {
+				hand.Kind = 4
+			} else {
+				hand.Kind = 3
+			}
+		} else if len(hand.Set) == 2 {
+			if hand.Set[0].Count == 4 || hand.Set[1].Count == 4 {
+				hand.Kind = 6
+			} else {
+				hand.Kind = 5
+			}
+		} else if len(hand.Set) == 1 {
+			hand.Kind = 7
+		}
+	} else {
+		for _, card := range []string{"A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2"} {
+			if strings.Count(handString, card) > 0 {
+				hand.Set = append(hand.Set, struct {
+					Card  string
+					Count int
+				}{Card: card, Count: strings.Count(handString, card)})
+			}
+		}
+		hand.Kind = JokerFix(hand, Joker)
 	}
-	if len(hand.Set) == 5 {
-		hand.Kind = 1
-		hand.Label = "best card"
-	} else if len(hand.Set) == 4 {
-		hand.Kind = 2
-		hand.Label = "two of a kind"
-	} else if len(hand.Set) == 3 {
-		if hand.Set[0].Count == 3 || hand.Set[1].Count == 3 || hand.Set[2].Count == 3 {
-			hand.Kind = 4
-			hand.Label = "three of a kind"
-		} else {
-			hand.Kind = 3
-			hand.Label = "two pairs"
-		}
-	} else if len(hand.Set) == 2 {
-		if hand.Set[0].Count == 4 || hand.Set[1].Count == 4 {
-			hand.Kind = 6
-			hand.Label = "four of a kind"
-		} else {
-			hand.Kind = 5
-			hand.Label = "full house"
-		}
-	} else if len(hand.Set) == 1 {
-		hand.Kind = 7
-		hand.Label = "five of a kind"
-	}
+
 	for _, char := range handString {
 		switch string(char) {
 		case "A":
@@ -120,12 +128,50 @@ func ParseHand(input string) (hand Hand) {
 		case "Q":
 			hand.Card = append(hand.Card, 60)
 		case "J":
-			hand.Card = append(hand.Card, 59)
+			hand.Card = append(hand.Card, 10)
 		case "T":
 			hand.Card = append(hand.Card, 58)
 		default:
 			hand.Card = append(hand.Card, int(char))
 		}
 	}
+	hand.Label = handString
 	return hand
+}
+
+func JokerFix(hand Hand, joker int) (kind int) {
+	if joker == 5 {
+		kind = 7
+	} else if joker == 4 {
+		kind = 7
+	} else if joker == 3 {
+		if len(hand.Set) == 2 {
+			kind = 6
+		} else if len(hand.Set) == 1 {
+			kind = 7
+		}
+	} else if joker == 2 {
+		if len(hand.Set) == 1 {
+			kind = 7
+		} else if len(hand.Set) == 2 {
+			kind = 6
+		} else if len(hand.Set) == 3 {
+			kind = 4
+		}
+	} else if joker == 1 {
+		if len(hand.Set) == 1 {
+			kind = 7
+		} else if len(hand.Set) == 2 {
+			if hand.Set[0].Count == 3 || hand.Set[1].Count == 3 {
+				kind = 6
+			} else {
+				kind = 5
+			}
+		} else if len(hand.Set) == 3 {
+			kind = 4
+		} else if len(hand.Set) == 4 {
+			kind = 2
+		}
+	}
+	return kind
 }
